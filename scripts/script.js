ddb94fceb515090816aa0a8d4ab1df2ca7e2e194
manduae <60371726+manduae@users.noreply.github.com>
2021-10-29 15:08:00 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
 // JavaScript Document
+
+var menuList = document.getElementById("menuList");
+
+menuList.style.maxHeight = "0px";
+
+function togglemenu(){
+    
+    if(menuList.style.maxHeight == "0px")
+        {
+          menuList.style.maxHeight == "130px";  
+        }
+    else 
+        {
+            menuList.style.maxHeight == "0px";
+        }
+}
